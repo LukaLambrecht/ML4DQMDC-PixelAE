f8da3d38953e661e11c75a9f40e2add20b5aeb51
Luka Lambrecht <llambrec@cern.ch>
2022-03-09 14:47:42 +0100

    fix inconsistent tabs and spaces

--- dqmio/harvest_nanodqmio_to_root.py
+++ dqmio/harvest_nanodqmio_to_root.py
@@ -12,47 +12,47 @@ from DQMIOReader import DQMIOReader
 
 if __name__=='__main__':
 
-    # definitions
-    datasetname = '/MinimumBias/Commissioning2021-900GeVmkFit-v2/DQMIO'
-    # (name of the data set on DAS)
-    redirector = 'root://cms-xrd-global.cern.ch/'
-    # (redirector used to access remote files)
-    mename = 'PixelPhase1/Tracks/PXBarrel/chargeInner_PXLayer_1'
-    # (name of the monitoring element to store)
-    outputfile = 'test.root'
-    # (path to output file)
-    istest = True 
-    # (if set to true, only one file will be read for speed)
+  # definitions
+  datasetname = '/MinimumBias/Commissioning2021-900GeVmkFit-v2/DQMIO'
+  # (name of the data set on DAS)
+  redirector = 'root://cms-xrd-global.cern.ch/'
+  # (redirector used to access remote files)
+  mename = 'PixelPhase1/Tracks/PXBarrel/chargeInner_PXLayer_1'
+  # (name of the monitoring element to store)
+  outputfile = 'test.root'
+  # (path to output file)
+  istest = True 
+  # (if set to true, only one file will be read for speed)
 
-    # make and execute the DAS client command
-    print('running DAS client to find files in dataset {}...'.format(datasetname))
-    dascmd = "dasgoclient -query 'file dataset={}' --limit 0".format(datasetname)
-    dasstdout = os.popen(dascmd).read()
-    dasfiles = [el.strip(' \t') for el in dasstdout.strip('\n').split('\n')]
-    if istest: dasfiles = [dasfiles[0]] 
-    print('DAS client ready; found following files ({}):'.format(len(dasfiles)))
-    for f in dasfiles: print('  - {}'.format(f))
+  # make and execute the DAS client command
+  print('running DAS client to find files in dataset {}...'.format(datasetname))
+  dascmd = "dasgoclient -query 'file dataset={}' --limit 0".format(datasetname)
+  dasstdout = os.popen(dascmd).read()
+  dasfiles = [el.strip(' \t') for el in dasstdout.strip('\n').split('\n')]
+  if istest: dasfiles = [dasfiles[0]] 
+  print('DAS client ready; found following files ({}):'.format(len(dasfiles)))
+  for f in dasfiles: print('  - {}'.format(f))
 
-    # make a DQMIOReader instance and initialize it with the DAS files
-    print('initializing DQMIOReader...')
-    redirector = redirector.rstrip('/')+'/'
-    dasfiles = [redirector+f for f in dasfiles]
-    reader = DQMIOReader(*dasfiles)
-    reader.sortIndex()
-    print('initialized DQMIOReader with following properties')
-    print('number of lumisections: {}'.format(len(reader.listLumis())))
-    print('number of monitoring elements per lumisection: {}'.format(len(reader.listMEs())))
+  # make a DQMIOReader instance and initialize it with the DAS files
+  print('initializing DQMIOReader...')
+  redirector = redirector.rstrip('/')+'/'
+  dasfiles = [redirector+f for f in dasfiles]
+  reader = DQMIOReader(*dasfiles)
+  reader.sortIndex()
+  print('initialized DQMIOReader with following properties')
+  print('number of lumisections: {}'.format(len(reader.listLumis())))
+  print('number of monitoring elements per lumisection: {}'.format(len(reader.listMEs())))
 
-    # select the monitoring element
-    print('selecting monitoring element {}...'.format(mename))
-    mes = reader.getSingleMEs(mename)
+  # select the monitoring element
+  print('selecting monitoring element {}...'.format(mename))
+  mes = reader.getSingleMEs(mename)
     
-    # write selected monitoring elements to output file
-    print('writing output file...')
-    f = ROOT.TFile.Open(outputfile, 'recreate')
-    for me in mes:
-	name = 'run{}_ls{}_{}'.format(me.run, me.lumi, me.name.replace('/','_'))
-	me.data.SetName(name)
-	me.data.SetTitle(name)
-	me.data.Write()
-    f.Close()
+  # write selected monitoring elements to output file
+  print('writing output file...')
+  f = ROOT.TFile.Open(outputfile, 'recreate')
+  for me in mes:
+    name = 'run{}_ls{}_{}'.format(me.run, me.lumi, me.name.replace('/','_'))
+    me.data.SetName(name)
+    me.data.SetTitle(name)
+    me.data.Write()
+  f.Close()
